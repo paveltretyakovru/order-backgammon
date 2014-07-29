02dd32525dddf8ddc0b5d21e1de13956f4e01ddf
Pavel Tretyakov <tretjakov.pavel@gmail.com>
2014-07-29 19:04:28 +0400

    update system.js style.css

--- js/system.js
+++ js/system.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+	$('.top_field , .bottom_field').sortable({
+		connectWith : '.top_field , .bottom_field'
+	}).disableSelection();
+
 	window.System = {
 		// метод вызывает методы других объектов
 		completeEvent : function(object , method , params){
@@ -22,7 +26,7 @@ $(document).ready(function(){
 
 	window.Game = {
 		type : 'local' ,
-		status : 'Start' ,
+		status : 'lot' ,
 		
 		steps : {
 			player 	: [] ,
@@ -34,6 +38,11 @@ $(document).ready(function(){
 			if(new_player !== '' && new_title !== ''){
 				this.steps.player[this.steps.player.length] = new_player;
 				this.steps.title[this.steps.title.length] 	= new_title;
+				if(typeof(new_data) === 'object'){
+					this.steps.data[this.steps.data.length] = new_data;
+				}else{
+					this.steps.data[this.steps.data.length] = false;
+				}
 				if(new_player === 'Player1'){
 					Player1.count_steps++;
 				}else{
@@ -45,49 +54,88 @@ $(document).ready(function(){
 
 	window.Player1 = {
 		name : 'Player1' ,
-		count_steps : 0
+		count_steps : 0 ,
+		relot : false
 	};
 
 	window.Player2 = {
 		name : 'Player2' ,
-		count_steps : 0
+		count_steps : 0 ,
+		relot : false
 	};
 
 	window.Board = {
-		bone1 : $('#die1') ,
-		bone2 : $('#die2') ,
+		bone1_val : 0 ,
+		bone2_val : 0 ,
+
+		steps_player : '' ,
+
+		bone1_selector : $('#die1') ,
+		bone2_selector : $('#die2') ,
 
 		chakeBones : function(params){
-			switch(Game.type){
-				case 'local':
-					// Проверяем кидают ли игроки жребии
-					if(Player1.count_steps === 0){
-						// обновляем лог и заносим информацию игроками
-						Game.addStep('Player1' , 'throwlot');
+			switch(Game.status){
+				case 'lot':
+					// проверяем кинул ли первый игрок жребий
+					if(this.bone1_val === 0){
 						// трясем кости
-						this.chake(this.bone1);
-						// генерируем случайное число
-						var roll = Math.floor(Math.random() * 6) + 1;
-						// отображаем на костях нужное количество точек
-						this.bone1.addClass("active").attr('data-value' , roll);
-						console.log('Игрок 1 кинул жребий');
+						this.chake(this.bone1_selector);
 
-						return true;
-					}else if(Player2.count_steps === 0){
-						Game.addStep('Player2' , 'throwlot');
-						this.chake(this.bone2);
 						var roll = Math.floor(Math.random() * 6) + 1;
-						this.bone2.addClass("active").attr('data-value' , roll);
-						// оба игрока кинули жребий, меняем статус игры
-						Game.status = 'Game';
-						console.log('Игрок 2 кинул жребий');
+						// ставим точки на кости
+						this.bone1_selector.addClass("active").attr('data-value' , roll);
+						this.bone1_val = roll;
 
-						return true;
-					// если жребий брошен, значит начинаем игру
-					}else if(Game.status === 'Game'){
-						alert('Start game :)');
+					}else if(this.bone2_val === 0){
+						this.chake(this.bone2_selector);
+
+						var roll = Math.floor(Math.random() * 6) + 1;
+						this.bone2_selector.addClass("active").attr('data-value' , roll);
+						this.bone2_val = roll;
+
+						// Смотрим, какой игрок будет ходить
+						if(this.bone1_val > this.bone2_val){
+							this.steps_player = 'Player1';
+							Game.status = 'game';
+						}else if(this.bone1_val < this.bone2_val){
+							this.steps_player = 'Player2';
+							Game.status = 'game';
+						}else{
+							this.bone1_val = 0;
+							this.bone2_val = 0;
+						}
 					}
 				break;
+				case 'game' :
+					// трясем обе кости
+					this.chake($('#die1 , #die2'));
+					var roll 	= Math.floor(Math.random() * 6) + 1;
+					var roll1 	= Math.floor(Math.random() * 6) + 1;
+					this.bone1_selector.addClass("active").attr('data-value' , roll);
+					this.bone2_selector.addClass("active").attr('data-value' , roll1);
+
+					// меням ходящего игрока
+					if(this.steps_player === 'Player1'){
+						this.steps_player = 'Player2';
+						this.setPlayerActiv('Player1');
+						console.log('Ходит 1 игрок')
+					}else{
+						this.steps_player = 'Player1';
+						this.setPlayerActiv('Player2');
+						console.log('Ходит 2 игрок');
+					}
+
+					this.bone1_val = roll;
+					this.bone1_val = roll1;
+				break;
+			}
+		} ,
+
+		setPlayerActiv : function(player){
+			if(player === 'Player1'){
+								
+			}else{
+				
 			}
 		} ,
 
